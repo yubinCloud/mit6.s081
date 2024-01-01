dddb969a498ba386d770714c15a067538aba5a72
yubinCloud <yubin_SkyWalker@yeah.net>
2024-01-01 07:09:15 +0000

    lab 1: finish SLEEP

---
+++ user/sleep.c
@@ -0,0 +1,16 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+mian(int argc, char *argv[])
+{
+    if (argc < 2) {
+        fprintf(2, "Usage: sleep ticks...\n");
+        exit(1);
+    }
+
+    int ticks = atoi(argv[1]);
+    sleep(ticks);
+    exit(0);
+}
