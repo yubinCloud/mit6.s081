4b4ba0a3bbd7511ce0b8e2254b0fdbcdb5f69f69
yubinCloud <yubin_SkyWalker@yeah.net>
2024-02-01 08:53:53 +0000

    :white_check_mark: Lab 4: pass `backtrace`

--- kernel/printf.c
+++ kernel/printf.c
@@ -121,6 +121,7 @@ panic(char *s)
   printf("panic: ");
   printf(s);
   printf("\n");
+  backtrace();
   panicked = 1; // freeze uart output from other CPUs
   for(;;)
     ;
@@ -131,4 +132,16 @@ printfinit(void)
 {
   initlock(&pr.lock, "pr");
   pr.locking = 1;
+}
+
+void
+backtrace()
+{
+  printf("backtrace:\n");
+  uint64 fp = r_fp();   // FP 寄存器值
+  uint64 base = PGROUNDUP(fp);  // 栈底地址
+  while (fp < base) {  // 向前遍历 frame，直到达到 base
+    printf("%p\n", *((uint64*)(fp - 8)));  // 打印 Return Address 字段值
+    fp = *((uint64*)(fp - 16));  // prev frame
+  }
 }
