6e20f0604eba387a930b996ab505eab29735193e
yubinCloud <yubin_SkyWalker@yeah.net>
2024-03-14 08:49:07 +0000

    :white_check_mark: pass Lab locks task 1

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -18,15 +18,24 @@ struct run {
   struct run *next;
 };
 
-struct {
+struct Kmem {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+} kmems[NCPU];  // 每个 CPU 一个 kmem
+
+int freelist_size;  // 每个 kmem 上 freelist 的最初大小
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  // 初始化 kmems 的 lock
+  for (int i = 0; i < NCPU; i++) {
+    char buf[10];
+    snprintf(buf, 10, "kmem-%d", i);
+    initlock(&kmems[i].lock, buf);
+    kmems[i].freelist = 0;
+  }
+
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -35,8 +44,22 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
+  
+  // 计算 freelist_size
+  int page_num = 0;
+  for (; p + PGSIZE <= (char*)pa_end; p += PGSIZE) {
+    page_num++;
+  }
+  freelist_size = page_num / NCPU + 1;
+
+  p = (char*)PGROUNDUP((uint64)pa_start);
   for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
     kfree(p);
+}
+
+// 计算物理地址 pa 应该由哪个 kmem 的 freelist 来管
+int kmem_number(void* pa) {
+  return ( (uint64)pa - (uint64)end ) / PGSIZE / freelist_size;
 }
 
 // Free the page of physical memory pointed at by v,
@@ -47,6 +70,7 @@ void
 kfree(void *pa)
 {
   struct run *r;
+  struct Kmem* kmem;
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
@@ -55,11 +79,41 @@ kfree(void *pa)
   memset(pa, 1, PGSIZE);
 
   r = (struct run*)pa;
+  kmem = kmems + kmem_number(pa);
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  acquire(&kmem->lock);
+  r->next = kmem->freelist;
+  kmem->freelist = r;  // 把归还的 page 放到列表头部
+  release(&kmem->lock);
+}
+
+struct Kmem*
+find_freelist()
+{
+  int cpu_id = cpuid();
+  struct Kmem* kmem = kmems + cpu_id;
+
+  // 先检查自己的 freelist 是否能够分配
+  acquire(&kmem->lock);
+  if (kmem->freelist) {
+    return kmem;
+  }
+  release(&kmem->lock);
+
+  // 如果自己的 freelist 空了的话，就从其他人那里找
+  for (int i = 0; i < NCPU; i++) {
+    if (i == cpu_id) {
+      continue;
+    }
+    kmem = kmems + i;
+    acquire(&kmem->lock);
+    if (kmem->freelist) {
+      return kmem;
+    }
+    release(&kmem->lock);
+  }
+
+  return 0;
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -69,12 +123,17 @@ void *
 kalloc(void)
 {
   struct run *r;
-
-  acquire(&kmem.lock);
-  r = kmem.freelist;
+  
+  // 找到一个 freelist 不为空的 kmem
+  struct Kmem *kmem = find_freelist();
+  if (kmem == 0) {
+    return 0;
+  }
+  // 从 freelist 列表头部取出一个空闲的 page
+  r = kmem->freelist;
   if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    kmem->freelist = r->next;
+  release(&kmem->lock);
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
